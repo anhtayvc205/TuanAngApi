d224b202e755cb2f068e3eeb91b26f4ed5ea83f8
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 19:57:08 +0700

    Update AutoSaveTask.java

--- src/main/java/me/tuanang/api/AutoSaveTask.java
+++ src/main/java/me/tuanang/api/AutoSaveTask.java
@@ -1,22 +1,17 @@
 package me.tuanang.api;
 
-import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class AutoSaveTask extends BukkitRunnable {
 
     @Override
     public void run() {
-        // autosave cache
         PlayerCache.saveAll();
-        Bukkit.getLogger().info("[TuanAngApi] Auto saved player cache");
     }
 
     public static void start() {
         new AutoSaveTask().runTaskTimerAsynchronously(
-                TuanAngApi.getInstance(),
-                20L * 60,
-                20L * 60
+                TuanAngApi.getInstance(), 20 * 60, 20 * 60
         );
     }
 }
