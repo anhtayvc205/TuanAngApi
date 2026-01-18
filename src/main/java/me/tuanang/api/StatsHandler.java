40d1fe502f7a899cfb5c54007fff5a3ed8313d32
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 19:22:33 +0700

    Update StatsHandler.java

--- src/main/java/me/tuanang/api/StatsHandler.java
+++ src/main/java/me/tuanang/api/StatsHandler.java
@@ -1,8 +1,34 @@
-JSONObject json = new JSONObject();
-json.put("name", data.name);
-json.put("break", data.breakBlock);
-json.put("place", data.placeBlock);
-json.put("kill", data.kill);
-json.put("death", data.death);
-json.put("playtime", data.formatPlaytime());
-json.put("lastSeen", data.lastSeenText());
+package me.tuanang.api;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import org.json.JSONObject;
+
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class StatsHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange ex) {
+        try {
+            String name = ex.getRequestURI().getPath().replace("/stats/", "");
+            PlayerData d = PlayerCache.get(name);
+
+            JSONObject json = new JSONObject();
+            json.put("name", d.name);
+            json.put("break", d.breakBlock);
+            json.put("place", d.placeBlock);
+            json.put("kill", d.kill);
+            json.put("death", d.death);
+            json.put("playtime", Utils.formatTime(d.playtime));
+            json.put("lastSeen", Utils.lastSeen(d));
+
+            byte[] resp = json.toString().getBytes(StandardCharsets.UTF_8);
+            ex.getResponseHeaders().add("Content-Type", "application/json; charset=utf-8");
+            ex.sendResponseHeaders(200, resp.length);
+            OutputStream os = ex.getResponseBody();
+            os.write(resp);
+            os.close();
+        } catch (Exception ignored) {}
+    }
+}
