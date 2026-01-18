081eefc69d3179616d2fc5a816138322e366513e
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 14:17:07 +0700

    Update PlayerListener.java

--- src/main/java/me/tuanang/api/PlayerListener.java
+++ src/main/java/me/tuanang/api/PlayerListener.java
@@ -1,65 +1,26 @@
-package me.tuanang.api;
+@EventHandler
+public void onJoin(PlayerJoinEvent e) {
+    plugin.cache.onJoin(e.getPlayer());
+}
 
-import org.bukkit.event.Listener;
-import org.bukkit.event.EventHandler;
+@EventHandler
+public void onQuit(PlayerQuitEvent e) {
+    plugin.cache.onQuit(e.getPlayer());
+}
 
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
+@EventHandler
+public void onBreak(BlockBreakEvent e) {
+    plugin.cache.addBreak(e.getPlayer().getUniqueId());
+}
 
-import org.bukkit.event.entity.EntityDeathEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
+@EventHandler
+public void onPlace(BlockPlaceEvent e) {
+    plugin.cache.addPlace(e.getPlayer().getUniqueId());
+}
 
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-
-import java.util.UUID;
-
-public class PlayerListener implements Listener {
-
-    private final TuanAngApi plugin;
-
-    public PlayerListener(TuanAngApi plugin) {
-        this.plugin = plugin;
-    }
-
-    @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        UUID u = e.getPlayer().getUniqueId();
-        plugin.CACHE.update(u, "lastSeen", System.currentTimeMillis());
-    }
-
-    @EventHandler
-    public void onQuit(PlayerQuitEvent e) {
-        UUID u = e.getPlayer().getUniqueId();
-        plugin.CACHE.update(u, "lastSeen", System.currentTimeMillis());
-    }
-
-    @EventHandler
-    public void onBlockPlace(BlockPlaceEvent e) {
-        UUID u = e.getPlayer().getUniqueId();
-        plugin.CACHE.update(u, "place",
-                plugin.CACHE.getLong(u, "place") + 1);
-    }
-
-    @EventHandler
-    public void onBlockBreak(BlockBreakEvent e) {
-        UUID u = e.getPlayer().getUniqueId();
-        plugin.CACHE.update(u, "break",
-                plugin.CACHE.getLong(u, "break") + 1);
-    }
-
-    @EventHandler
-    public void onPlayerDeath(PlayerDeathEvent e) {
-        UUID u = e.getEntity().getUniqueId();
-        plugin.CACHE.update(u, "death",
-                plugin.CACHE.getLong(u, "death") + 1);
-    }
-
-    @EventHandler
-    public void onKill(EntityDeathEvent e) {
-        if (e.getEntity().getKiller() == null) return;
-        UUID u = e.getEntity().getKiller().getUniqueId();
-        plugin.CACHE.update(u, "kill",
-                plugin.CACHE.getLong(u, "kill") + 1);
-    }
+@EventHandler
+public void onDeath(PlayerDeathEvent e) {
+    plugin.cache.addDeath(e.getEntity().getUniqueId());
+    if (e.getEntity().getKiller() != null)
+        plugin.cache.addKill(e.getEntity().getKiller().getUniqueId());
 }
