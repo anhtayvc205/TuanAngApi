b19fc7e8e87030fe55f29311019aec22e7cca327
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 12:50:57 +0700

    Update TuanAngApi.java

--- src/main/java/me/tuanang/api/TuanAngApi.java
+++ src/main/java/me/tuanang/api/TuanAngApi.java
@@ -1,7 +1,6 @@
 package me.tuanang.api;
 
 import com.sun.net.httpserver.HttpServer;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.net.InetSocketAddress;
@@ -10,18 +9,18 @@ public class TuanAngApi extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        int port = 25673; // nhớ mở port này trong HyperCore
+        int port = 25673;
 
         try {
             HttpServer server = HttpServer.create(
-                    new InetSocketAddress("127.0.0.1", port), 0
+                    new InetSocketAddress(port), 0
             );
 
             server.createContext("/stats", new StatsHandler(this));
             server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
             server.start();
 
-            getLogger().info("API started on port " + port);
+            getLogger().info("API started on 0.0.0.0:" + port);
 
         } catch (Exception e) {
             e.printStackTrace();
