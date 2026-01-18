32486634e296ecbfb9e406c57b6da3b1f7e591b9
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 19:54:13 +0700

    Update TuanAngApi.java

--- src/main/java/me/tuanang/api/TuanAngApi.java
+++ src/main/java/me/tuanang/api/TuanAngApi.java
@@ -1,41 +1,37 @@
 package me.tuanang.api;
 
 import com.sun.net.httpserver.HttpServer;
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.net.InetSocketAddress;
-import java.util.concurrent.Executors;
 
 public class TuanAngApi extends JavaPlugin {
 
-    public static TuanAngApi instance;
-    public PlayerCache cache;
+    private static TuanAngApi instance;
+
+    public static TuanAngApi getInstance() {
+        return instance;
+    }
 
     @Override
     public void onEnable() {
         instance = this;
-        cache = new PlayerCache();
 
-        Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+        AutoSaveTask.start();
 
-        startApi(25673);
-
-        // autosave mỗi 10s
-        Bukkit.getScheduler().runTaskTimerAsynchronously(this,
-                cache::saveAll, 200L, 200L);
-    }
-
-    private void startApi(int port) {
         try {
-            HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", port), 0);
+            HttpServer server = HttpServer.create(new InetSocketAddress(25673), 0);
             server.createContext("/stats", new StatsHandler());
-            server.setExecutor(Executors.newCachedThreadPool());
             server.start();
-
-            getLogger().info("API started on port " + port);
+            getLogger().info("API started :25673");
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public void onDisable() {
+        PlayerCache.saveAll();
     }
 }
