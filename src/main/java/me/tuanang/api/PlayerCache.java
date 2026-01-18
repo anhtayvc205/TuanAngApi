71b85f241061d118910f76436b33fb95f4512ab3
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 14:44:08 +0700

    Update PlayerCache.java

--- src/main/java/me/tuanang/api/PlayerCache.java
+++ src/main/java/me/tuanang/api/PlayerCache.java
@@ -1,32 +1,11 @@
 package me.tuanang.api;
 
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class PlayerCache {
+    public ConcurrentHashMap<String, PlayerData> data = new ConcurrentHashMap<>();
 
-    public static Map<String, PlayerCache> cache = new ConcurrentHashMap<>();
-
-    public String name;
-    public boolean online;
-    public long playtime; // seconds
-    public long lastSeen; // unix
-    public int blockPlace;
-    public int blockBreak;
-    public int kill;
-    public int death;
-
-    public PlayerCache(String name) {
-        this.name = name;
-    }
-
-    public void tick() {
-        if (online) {
-            playtime += 10;
-        }
-    }
-
-    public static PlayerCache get(String name) {
-        return cache.computeIfAbsent(name.toLowerCase(), PlayerCache::new);
+    public PlayerData get(String name) {
+        return data.computeIfAbsent(name.toLowerCase(), k -> new PlayerData());
     }
 }
