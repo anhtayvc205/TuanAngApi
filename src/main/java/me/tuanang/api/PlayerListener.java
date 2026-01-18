164d9c030f83f42e8400e281a8ee6d940f09d72b
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 19:42:21 +0700

    Update PlayerListener.java

--- src/main/java/me/tuanang/api/PlayerListener.java
+++ src/main/java/me/tuanang/api/PlayerListener.java
@@ -1,20 +1,40 @@
 package me.tuanang.api;
 
-import org.bukkit.event.*;
-import org.bukkit.event.player.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerListener implements Listener {
 
     @EventHandler
     public void onJoin(PlayerJoinEvent e) {
-        PlayerData d = PlayerCache.get(e.getPlayer().getName());
-        d.online = true;
+        var d = TuanAngApi.instance.cache.get(e.getPlayer().getName());
+        d.lastSeen = System.currentTimeMillis();
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
-        PlayerData d = PlayerCache.get(e.getPlayer().getName());
-        d.online = false;
+        var d = TuanAngApi.instance.cache.get(e.getPlayer().getName());
         d.lastSeen = System.currentTimeMillis();
+    }
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent e) {
+        TuanAngApi.instance.cache.get(e.getPlayer().getName()).breakBlock++;
+    }
+
+    @EventHandler
+    public void onPlace(BlockPlaceEvent e) {
+        TuanAngApi.instance.cache.get(e.getPlayer().getName()).placeBlock++;
+    }
+
+    @EventHandler
+    public void onKill(EntityDeathEvent e) {
+        if (e.getEntity().getKiller() == null) return;
+        TuanAngApi.instance.cache.get(e.getEntity().getKiller().getName()).mobKill++;
     }
 }
