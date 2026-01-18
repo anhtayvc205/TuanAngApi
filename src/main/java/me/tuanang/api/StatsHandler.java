71a14fdda0edee6cd639871d77c061bdb17478c8
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 19:12:19 +0700

    Update StatsHandler.java

--- src/main/java/me/tuanang/api/StatsHandler.java
+++ src/main/java/me/tuanang/api/StatsHandler.java
@@ -1,43 +1,8 @@
-package me.tuanang.api;
-
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.google.gson.JsonObject;
-
-import java.io.OutputStream;
-
-public class StatsHandler implements HttpHandler {
-
-    private final TuanAngApi plugin;
-
-    public StatsHandler(TuanAngApi plugin) {
-        this.plugin = plugin;
-    }
-
-    @Override
-    public void handle(HttpExchange e) {
-        try {
-            String name = e.getRequestURI().getPath().replace("/stats/", "");
-            PlayerData d = plugin.cache.data.get(name.toLowerCase());
-
-            JsonObject o = new JsonObject();
-            if (d == null) {
-                o.addProperty("error", "Người chơi chưa từng vào server");
-            } else {
-                o.addProperty("name", name);
-                o.addProperty("break", d.blockBreak);
-                o.addProperty("place", d.blockPlace);
-                o.addProperty("kill", d.kill);
-                o.addProperty("death", d.death);
-                o.addProperty("playtime", Utils.time(d.playtime));
-                o.addProperty("lastSeen", Utils.lastSeen(d.lastSeen));
-            }
-
-            byte[] res = o.toString().getBytes();
-            e.sendResponseHeaders(200, res.length);
-            OutputStream os = e.getResponseBody();
-            os.write(res);
-            os.close();
-        } catch (Exception ignored) {}
-    }
-}
+JSONObject json = new JSONObject();
+json.put("name", data.name);
+json.put("break", data.breakBlock);
+json.put("place", data.placeBlock);
+json.put("kill", data.kill);
+json.put("death", data.death);
+json.put("playtime", data.formatPlaytime());
+json.put("lastSeen", data.lastSeenText());
