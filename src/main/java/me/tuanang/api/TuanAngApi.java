ef45df09fe3ed1e3db66b079270ccd7f15d1a2cc
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 13:44:03 +0700

    Update TuanAngApi.java

--- src/main/java/me/tuanang/api/TuanAngApi.java
+++ src/main/java/me/tuanang/api/TuanAngApi.java
@@ -4,36 +4,32 @@ import com.sun.net.httpserver.HttpServer;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.UUID;
+import java.util.concurrent.Executors;
 
 public class TuanAngApi extends JavaPlugin {
 
-    public Database db;
-    public HashMap<UUID, Long> joinTime = new HashMap<>();
+    public PlayerCache cache;
 
     @Override
     public void onEnable() {
+        cache = new PlayerCache(this);
 
-        db = new Database();
-        db.connect();
-
-        getServer().getPluginManager().registerEvents(
-                new PlayerListener(this), this
+        // Auto refresh mỗi 10s
+        getServer().getScheduler().runTaskTimer(
+                this,
+                () -> cache.refreshOnlinePlayers(),
+                200L, 200L
         );
-
-        int port = 25673;
 
         try {
             HttpServer server = HttpServer.create(
-                    new InetSocketAddress("0.0.0.0", port), 0
+                    new InetSocketAddress("0.0.0.0", 25673), 0
             );
-
             server.createContext("/stats", new StatsHandler(this));
-            server.setExecutor(java.util.concurrent.Executors.newCachedThreadPool());
+            server.setExecutor(Executors.newCachedThreadPool());
             server.start();
 
-            getLogger().info("API started on 0.0.0.0:" + port);
+            getLogger().info("API chạy tại :25673");
 
         } catch (Exception e) {
             e.printStackTrace();
