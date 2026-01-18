5f183a829914a9edb6b1996ab704f7773350ea9e
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 19:13:13 +0700

    Update TuanAngApi.java

--- src/main/java/me/tuanang/api/TuanAngApi.java
+++ src/main/java/me/tuanang/api/TuanAngApi.java
@@ -1,38 +1,5 @@
-package me.tuanang.api;
-
-import com.sun.net.httpserver.HttpServer;
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.java.JavaPlugin;
-
-import java.net.InetSocketAddress;
-
-public class TuanAngApi extends JavaPlugin {
-
-    public PlayerCache cache;
-    public StatsHandler api;
-
-    @Override
-    public void onEnable() {
-        cache = new PlayerCache();
-
-        Utils.load(this);
-
-        Bukkit.getPluginManager().registerEvents(new PlayerListener(this), this);
-        new AutoSaveTask(this).runTaskTimerAsynchronously(this, 200L, 200L);
-
-        try {
-            HttpServer server = HttpServer.create(new InetSocketAddress(25673), 0);
-            api = new StatsHandler(this);
-            server.createContext("/stats", api);
-            server.start();
-            getLogger().info("API started :25673");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onDisable() {
-        Utils.save(this);
-    }
+@Override
+public void onEnable() {
+    Bukkit.getPluginManager().registerEvents(new PlayerListener(), this);
+    new AutoSaveTask().runTaskTimer(this, 20L, 20L); // mỗi giây
 }
