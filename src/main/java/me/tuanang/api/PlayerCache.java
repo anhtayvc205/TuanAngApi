ac3203c6969b7623b395df7fc20599613ed49770
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 14:22:36 +0700

    Update PlayerCache.java

--- src/main/java/me/tuanang/api/PlayerCache.java
+++ src/main/java/me/tuanang/api/PlayerCache.java
@@ -1,29 +1,14 @@
+package me.tuanang.api;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 public class PlayerCache {
+    public ConcurrentHashMap<String, PlayerData> data = new ConcurrentHashMap<>();
 
-    private final Map<UUID, PlayerData> map = new ConcurrentHashMap<>();
-    private final Database db;
-
-    public PlayerCache(Database db) {
-        this.db = db;
-    }
-
-    public void onJoin(Player p) {
-        map.put(p.getUniqueId(), db.load(p.getUniqueId(), p.getName()));
-    }
-
-    public void onQuit(Player p) {
-        PlayerData d = map.remove(p.getUniqueId());
-        d.lastSeen = System.currentTimeMillis();
-        db.save(d);
-    }
-
-    public PlayerData get(String name) {
-        return map.values().stream()
-                .filter(d -> d.name.equalsIgnoreCase(name))
-                .findFirst().orElse(db.loadByName(name));
-    }
-
-    public void flushAll() {
-        map.values().forEach(db::save);
+    public static class PlayerData {
+        public long joinTime;
+        public long playtime;
+        public int breakBlock, placeBlock, kill, death;
+        public long lastSeen;
     }
 }
