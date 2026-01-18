5e32d54c89084594f4d51a79c6305ab58c6109ea
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 14:43:33 +0700

    Update and rename Database.java to PlayerData.java

---
+++ src/main/java/me/tuanang/api/PlayerData.java
@@ -0,0 +1,10 @@
+package me.tuanang.api;
+
+public class PlayerData {
+    public long playtime = 0;
+    public int blockBreak = 0;
+    public int blockPlace = 0;
+    public int kill = 0;
+    public int death = 0;
+    public long lastSeen = 0;
+}
