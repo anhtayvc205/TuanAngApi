e8636f3c2c4163ac3208f34ceef9bd5023160fc3
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 12:38:17 +0700

    Update TuanAngApi.java

--- src/main/java/me/tuanang/api/TuanAngApi.java
+++ src/main/java/me/tuanang/api/TuanAngApi.java
@@ -1,31 +1,32 @@
 package me.tuanang.api;
 
 import com.sun.net.httpserver.HttpServer;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.net.InetSocketAddress;
 
 public class TuanAngApi extends JavaPlugin {
 
+    public static TuanAngApi instance;
+
     @Override
     public void onEnable() {
-        saveDefaultConfig();
+        instance = this;
 
-        int port = getConfig().getInt("port");
-        String bind = getConfig().getString("bind");
+        int port = 25673;
 
         try {
-            HttpServer server = HttpServer.create(
-                    new InetSocketAddress(bind, port), 0);
-
-            server.createContext("/stats", new StatsHandler(this));
+            HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", port), 0);
+            server.createContext("/stats", new StatsHandler());
             server.setExecutor(null);
             server.start();
 
-            getLogger().info("API started at " + bind + ":" + port);
-
+            getLogger().info("API started on port " + port);
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        Bukkit.getPluginManager().registerEvents(new BlockListener(), this);
     }
 }
