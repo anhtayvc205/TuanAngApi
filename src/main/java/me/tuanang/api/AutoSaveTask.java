0222eeb60d288df9b26877eb86ac8945f76fa5ad
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 19:50:34 +0700

    Update AutoSaveTask.java

--- src/main/java/me/tuanang/api/AutoSaveTask.java
+++ src/main/java/me/tuanang/api/AutoSaveTask.java
@@ -1,14 +1,3 @@
-package me.tuanang.api;
-
-public class AutoSaveTask implements Runnable {
-    @Override
-    public void run() {
-        long now = System.currentTimeMillis();
-        for (PlayerData d : PlayerCache.all().values()) {
-            if (d.online) {
-                d.playtime += 10; // +10s
-                d.lastSeen = now;
-            }
-        }
-    }
+for (PlayerCache.Data d : PlayerCache.all()) {
+    // autosave
 }
