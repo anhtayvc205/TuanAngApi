3271f6516f40581fa1ab431b8640b9d1c2a5727c
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 14:23:59 +0700

    Update PlayerListener.java

--- src/main/java/me/tuanang/api/PlayerListener.java
+++ src/main/java/me/tuanang/api/PlayerListener.java
@@ -1,26 +1,47 @@
-@EventHandler
-public void onJoin(PlayerJoinEvent e) {
-    plugin.cache.onJoin(e.getPlayer());
-}
+package me.tuanang.api;
 
-@EventHandler
-public void onQuit(PlayerQuitEvent e) {
-    plugin.cache.onQuit(e.getPlayer());
-}
+import org.bukkit.event.*;
+import org.bukkit.event.player.*;
+import org.bukkit.event.block.*;
+import org.bukkit.event.entity.*;
+import org.bukkit.entity.Player;
 
-@EventHandler
-public void onBreak(BlockBreakEvent e) {
-    plugin.cache.addBreak(e.getPlayer().getUniqueId());
-}
+public class PlayerListener implements Listener {
 
-@EventHandler
-public void onPlace(BlockPlaceEvent e) {
-    plugin.cache.addPlace(e.getPlayer().getUniqueId());
-}
+    private final TuanAngApi plugin;
 
-@EventHandler
-public void onDeath(PlayerDeathEvent e) {
-    plugin.cache.addDeath(e.getEntity().getUniqueId());
-    if (e.getEntity().getKiller() != null)
-        plugin.cache.addKill(e.getEntity().getKiller().getUniqueId());
+    public PlayerListener(TuanAngApi plugin) {
+        this.plugin = plugin;
+    }
+
+    private PlayerCache.PlayerData get(Player p) {
+        return plugin.cache.data.computeIfAbsent(
+                p.getName().toLowerCase(),
+                k -> new PlayerCache.PlayerData()
+        );
+    }
+
+    @EventHandler
+    public void join(PlayerJoinEvent e) {
+        var d = get(e.getPlayer());
+        d.joinTime = System.currentTimeMillis();
+        d.lastSeen = 0;
+    }
+
+    @EventHandler
+    public void quit(PlayerQuitEvent e) {
+        var d = get(e.getPlayer());
+        d.playtime += (System.currentTimeMillis() - d.joinTime) / 1000;
+        d.lastSeen = System.currentTimeMillis() / 1000;
+    }
+
+    @EventHandler public void breakB(BlockBreakEvent e){ get(e.getPlayer()).breakBlock++; }
+    @EventHandler public void placeB(BlockPlaceEvent e){ get(e.getPlayer()).placeBlock++; }
+
+    @EventHandler
+    public void death(PlayerDeathEvent e) {
+        get(e.getEntity()).death++;
+        if (e.getEntity().getKiller() != null)
+            get(e.getEntity().getKiller()).kill++;
+    }
 }
