a3df2b004b3e0a0dc4daf3ca0e8a00ac44135523
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 11:32:01 +0700

    Update TuanAngApi.java

--- src/main/java/me/tuanang/api/TuanAngApi.java
+++ src/main/java/me/tuanang/api/TuanAngApi.java
@@ -1,26 +1,67 @@
+
 package me.tuanang.api;
 
+import com.google.gson.Gson;
+import org.bukkit.Bukkit;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.plugin.java.JavaPlugin;
+import spark.Spark;
 
-public class TuanAngApi extends JavaPlugin {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
-    private ApiServer server;
+public final class TuanAngApi extends JavaPlugin {
+
+    private final Gson gson = new Gson();
 
     @Override
     public void onEnable() {
         saveDefaultConfig();
 
-        int port = getConfig().getInt("port", 25672);
-        String key = getConfig().getString("key", "tuanang123");
+        int port = getConfig().getInt("port");
+        String key = getConfig().getString("key");
 
-        server = new ApiServer(this, port, key);
-        server.start();
+        Spark.ipAddress("0.0.0.0");
+        Spark.port(port);
 
-        getLogger().info("API chạy tại port " + port);
+        Spark.get("/stats/:name", (req, res) -> {
+            if (!key.equals(req.queryParams("key"))) {
+                res.status(401);
+                return "{\"error\":\"invalid_key\"}";
+            }
+
+            String name = req.params(":name");
+
+            CompletableFuture<Map<String, Object>> future = new CompletableFuture<>();
+
+            Bukkit.getScheduler().runTask(this, () -> {
+                Map<String, Object> data = new HashMap<>();
+                OfflinePlayer off = Bukkit.getOfflinePlayer(name);
+
+                data.put("name", off.getName());
+                data.put("uuid", off.getUniqueId().toString());
+                data.put("online", off.isOnline());
+
+                if (off.isOnline()) {
+                    var p = off.getPlayer();
+                    data.put("blocks_mined", p.getStatistic(org.bukkit.Statistic.MINE_BLOCK));
+                    data.put("deaths", p.getStatistic(org.bukkit.Statistic.DEATHS));
+                    data.put("play_time", p.getStatistic(org.bukkit.Statistic.PLAY_ONE_MINUTE) / 20);
+                }
+
+                future.complete(data);
+            });
+
+            res.type("application/json");
+            return gson.toJson(future.get());
+        });
+
+        getLogger().info("API started on port " + port);
     }
 
     @Override
     public void onDisable() {
-        if (server != null) server.stop();
+        Spark.stop();
     }
 }
