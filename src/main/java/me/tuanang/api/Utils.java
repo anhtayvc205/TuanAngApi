e8dbbd1e5546240fd1399f9b970019170530274a
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-18 19:23:07 +0700

    Update Utils.java

--- src/main/java/me/tuanang/api/Utils.java
+++ src/main/java/me/tuanang/api/Utils.java
@@ -1,5 +1,19 @@
+package me.tuanang.api;
+
 public class Utils {
-    public static String safe(String s) {
-        return s == null ? "0" : s;
+
+    public static String formatTime(long sec) {
+        long h = sec / 3600;
+        long m = (sec % 3600) / 60;
+        long s = sec % 60;
+        return h + "h" + m + "m" + s + "s";
+    }
+
+    public static String lastSeen(PlayerData d) {
+        if (d.online) return "đang online";
+        long diff = (System.currentTimeMillis() - d.lastSeen) / 1000;
+        if (diff < 60) return diff + " giây trước";
+        if (diff < 3600) return (diff / 60) + " phút trước";
+        return (diff / 3600) + " giờ trước";
     }
 }
